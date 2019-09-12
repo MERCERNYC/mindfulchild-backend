29776785641fae5613ccadaf0755f5d833900f02
Juliana Fogg <julianafogg@Julianas-MBP.fios-router.home>
2019-09-11 21:53:59 -0400

    setup controller with actions

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -1,2 +1,29 @@
-class ActivitiesController < ApplicationController
+class Api::V1::ActivitiesController < ApplicationController
+    
+    def index
+        @activities = @category.activities
+        render json: @activities
+    end 
+
+    # def create
+    #     @activity = @category.activities.build(activity_params)
+       
+    # end 
+
+    def show
+        @activity = Activity.find(params[:id])
+        render json: @activity
+    end 
+
+    private
+
+    def set_category
+        @category = Category.find(params[:category_id])
+    end 
+
+    def activity_params
+        params.require(:activity).permit(:name, :description, :category_id, :url)
+    end 
+
+end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
     has_many  :activities
-
     validates :title, presence: true
+
+    
      
 end
